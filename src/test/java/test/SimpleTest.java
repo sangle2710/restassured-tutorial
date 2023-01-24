dba598f4f37df3545d0a62c7cae9f53ba6b1fc9f
sangle2710 <sangle.qc.98@gmail.com>
2023-01-24 15:44:33 +0700

    GET method, send request and verify response

--- src/test/java/test/SimpleTest.java
+++ src/test/java/test/SimpleTest.java
@@ -1,4 +1,27 @@
 package test;
 
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class SimpleTest {
+    public static void main(String[] args) {
+        String baseUri = "https://jsonplaceholder.typicode.com/";
+
+        // Request scope
+        RequestSpecification request = given();
+        request.baseUri(baseUri);
+        request.basePath("todos");
+
+        // Response scope
+        final String FIRST_TODO = "1";
+        Response response = request.get(FIRST_TODO);
+        response.prettyPrint();
+        response.then().body("userId", equalTo(1));
+        response.then().body("id", equalTo(1));
+        response.then().body("title", equalTo("delectus aut autem"));
+        response.then().body("completed", equalTo(false));
+    }
 }
